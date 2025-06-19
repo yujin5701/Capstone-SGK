d1fe617b4cef43eba1e9f765afc3e143937f3021
DESKTOP-0C0AIIE\yooji <yoojin3117@ewhain.net>
2025-06-20 01:45:53 +0900

    ...

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -16,6 +16,7 @@ const upload = multer({
     s3,
     bucket: process.env.AWS_S3_BUCKET,
     // acl: "public-read", // S3에 퍼블릭 접근 가능
+    contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: (req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
     },
